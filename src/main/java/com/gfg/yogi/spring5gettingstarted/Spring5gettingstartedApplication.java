1fefd6ec84310e3e95eb024c2c2f659c5dfd7c47
yougeshwer Dan Charan <danchara@adobe.com>
2019-06-09 08:49:04 +0530

    Downloaded zip from spring initializr

---
+++ src/main/java/com/gfg/yogi/spring5gettingstarted/Spring5gettingstartedApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.yogi.spring5gettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5gettingstartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5gettingstartedApplication.class, args);
+	}
+
+}
